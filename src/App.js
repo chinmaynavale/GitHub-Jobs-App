408064310d3171a33bc5850101f1bd25586c8395
chinmaynavale <chinmaymnavale@gmail.com>
2020-09-04 17:01:48 +0530

    added Search functionality to our app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import useFetchJobs from './useFetchJobs';
 import Job from './Job';
 import JobsPagination from './JobsPagination';
+import SearchForm from './SearchForm';
 
 import { Container } from 'react-bootstrap';
 import './App.css';
@@ -11,9 +12,17 @@ function App() {
   const [page, setPage] = useState(1);
   const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
 
+  const handleParamChange = (e) => {
+    const param = e.target.name;
+    const value = e.target.value;
+    setPage(1);
+    setParams((prevParams) => ({ ...prevParams, [param]: value }));
+  };
+
   return (
     <Container className="py-4">
       <h1 className="mb-4">GitHub Jobs</h1>
+      <SearchForm params={params} onParamChange={handleParamChange} />
       <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try Refreshing.</h1>}
